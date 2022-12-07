0ef6a88b7d92606a2c6ddf0c274d647eb134e51d
satoshi-testaccount <105473484+satoshi-testaccount@users.noreply.github.com>
2022-12-07 00:06:44 -0600

    Add files via upload

---
+++ js/navbar.js
@@ -0,0 +1,9 @@
+﻿
+function toggleNavbar() {
+  var nav = document.getElementById("toggle-nav");
+  if (nav.className === "navbar") {
+    nav.className += " responsive";
+  } else {
+    nav.className = "navbar";
+  }
+}
---
+++ js/title.js
@@ -0,0 +1,13 @@
+﻿let rek = document.querySelectorAll(".text_input");
+let date= document.querySelector(".date");
+window.addEventListener("load", function(){
+	addTitle();
+});
+
+function addTitle(){
+	for (i=0; i < rek.length; i++){
+		rek[i].setAttribute("title","This field is required");
+		rek[i].required = "required";
+	}
+	
+}
